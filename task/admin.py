04b35744ebbaf527a127436874456c4fff493a74
ayman <ayman.h.97abu-eideh@hotmail.com>
2022-01-27 13:01:19 +0200

    build the models

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+# from django.contrib.admin import register, ModelAdmin
+from task.models import User, Checkpoint, Claim, SubscribedPoints
 
 # Register your models here.
+
+admin.site.site_header = 'Harri Task'
+admin.site.index_title = 'Harri Task'
+admin.site.site_title = 'Harri Task'
+admin.site.register(User)
+admin.site.register(Checkpoint)
+admin.site.register(Claim)
+admin.site.register(SubscribedPoints)
+
+# @register(User)
+# class UserAdmin(ModelAdmin):
--- task/models.py
+++ task/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class Checkpoint(models.Model):
+    location_name = models.CharField(max_length=128, blank=False, null=False)
+    latitude = models.FloatField(blank=False, null=False)
+    longitude = models.FloatField(blank=False, null=False)
+    min_number = models.IntegerField(blank=False, null=False)
+
+
+class User(models.Model):
+    email = models.CharField(max_length=50, blank=False, null=False)
+    user_name = models.CharField(max_length=50, blank=False, null=False)
+    password = models.CharField(max_length=50, blank=False, null=False)
+    trusted = models.BooleanField(blank=False, null=False)
+
+
+class Claim(models.Model):
+    user = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE,
+                             related_name="claim_user_id")
+    checkpoint = models.ForeignKey(Checkpoint, blank=False, null=False, on_delete=models.CASCADE,
+                                   related_name="claim_checkpoint_id")
+    state = models.BooleanField(blank=False, null=False)
+    timestamp = models.BigIntegerField(blank=False, null=False)
+
+
+class SubscribedPoints(models.Model):
+    user = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE,
+                             related_name="subscribed_user_id")
+    checkpoint = models.ForeignKey(Checkpoint, blank=False, null=False, on_delete=models.CASCADE,
+                                   related_name="subscribed_checkpoint_id")
