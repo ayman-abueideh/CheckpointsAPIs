7b02e07af10f6ca9bea2b21c0143096ae90f5fc9
ayman <ayman.h.97abu-eideh@hotmail.com>
2022-01-27 17:49:02 +0200

    rename project name

--- CheckPointsTask/urls.py
+++ CheckPointsTask/urls.py
@@ -1,4 +1,4 @@
-"""HarriTask URL Configuration
+"""CheckPointsTask URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
